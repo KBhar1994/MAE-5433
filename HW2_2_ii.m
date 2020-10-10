485d650ad59ae83e843aa2fa1c159c7e557522e1
KBhar1994 <60757573+KBhar1994@users.noreply.github.com>
2020-10-09 20:01:24 -0500

    Add files via upload

--- HW2_2_ii.m
+++ HW2_2_ii.m
@@ -8,9 +8,9 @@ syms l1 l2 l3 'real';
 x = [1 0 0]';
 y = [0 1 0]';
 z = [0 0 1]';
-H = [z y 1/sqrt(l1^2+l2^2)*(x+z)];
+H = [z y 1/sqrt(l1^2+l2^2)*(l2*x+l1*z)];
 syms P [3 n]  real;
-P = [[0 0 0]' [0 0 0]' [0 0 -l1]'];
+P = [[0 0 0]' [0 0 0]' [l2 0 0]'];
 
 [R,p] = fwdkin(q,type,H,P,n);
 
